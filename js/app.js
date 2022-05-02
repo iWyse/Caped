5e1a1f4abbfb986794753fde04fb0c413aa7bf19
Wyse <originalwyse@gmail.com>
2022-05-02 14:35:41 +0300

    fixed scroll, href, padding

--- js/app.js
+++ js/app.js
@@ -13,6 +13,41 @@
             document.documentElement.classList.add(className);
         }));
     }
+    let bodyLockStatus = true;
+    let bodyUnlock = (delay = 100) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
+                }
+                body.style.paddingRight = "0px";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 100) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            }
+            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
     const menu = document.querySelector(".menu");
     menu.querySelector(".menu-list");
     menu.querySelector(".menu-mobile-arrow");
@@ -30,6 +65,7 @@
     }));
     function toggleMenu() {
         menu.classList.toggle("active");
+        if (menu.classList.contains("active")) bodyLock(); else bodyUnlock();
         menuOverlay.classList.toggle("active");
     }
     window.onresize = function() {
